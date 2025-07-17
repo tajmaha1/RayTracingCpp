d5ceec92d090ef58eecd1b3ed197226e8e12cc10
megatr4n <dahiildahiil07@gmail.com>
2025-07-17 14:01:06 +0300

    add build.sh for CMake and manual compile

--- build.sh
+++ build.sh
@@ -19,3 +19,23 @@ mkdir -p images
 clang++ -std=c++17 -I include -o RayTracingCpp src/*.cpp
 ./RayTracingCpp > images/image.ppm
 magick images/image.ppm images/output.png
+
+
+
+To convert .ppm files to .png, the project uses ImageMagick's magick command.
+
+If magick is not installed, install it as follows:
+
+macOS (using Homebrew):
+
+brew install imagemagick
+
+Windows:
+
+Download and install from the official website:
+https://imagemagick.org/script/download.php#windows
+
+After installing magick, you can convert .ppm to .png using:
+
+magick images/image.ppm images/output.png
+
